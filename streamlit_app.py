b43f11e02bcf6bd7cf767900126b99ea46121e20
Kaushik Roy <kaushik.roy1984@gmail.com>
2022-07-02 22:47:52 -0400

    get dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,107 +60,43 @@ st.write('Count how many of ',topic,' are available.')
 
 # set level in query
 if level == 'Country':
-    level_query = 'select country_code code,country_name name, geometry from dev-ind-geo-01.geoprocessed.country'
+
     pass
 elif level == 'States':
-    level_query = 'select state_code code,state_name name, geometry from dev-ind-geo-01.geoprocessed.states;'
+
     pass
 elif level == 'Districts':
-    level_query = 'select district_code code,district_name name, geometry from dev-ind-geo-01.geoprocessed.districts'
+
     pass
 elif level == 'Subdistricts':
-    level_query = 'select taluk_code code,taluk_name name, geometry from dev-ind-geo-01.geoprocessed.subdistricts'
+
     pass
 elif level == 'Parlamentary Constituencies':
-    level_query = 'select * from Parlamentary Constituencies'
     pass
 elif level == 'Assembly Consituencies':
-    level_query = 'select * from Assembly Consituencies'
     pass
-
-def get_run_query(topic,level_query):
-    run_query = """
-    select a.*,t.geometry 
-    from (
-    select code,name,count(cgeom) cnt
-    from(
-    select t.code,t.name,t.geometry tgeom,c.geometry cgeom from dev-ind-geo-01.geoprocessed."""+topic+""" c,("""+level_query+""") t
-    where ST_CONTAINS(t.geometry,h.geometry)
-    )
-    group by code,name) a right join ("""+level_query+""") t on t.code = a.code
-    """
-    return run_query
 
 
 # set topic in query
 if topic == 'Roads':
-    run_query = get_run_query(topic,level_query)
+
     pass
 elif topic == 'Habitations':
-    run_query = """
-    select a.*,t.geometry 
-    from (
-    select code,name,count(cgeom) cnt
-    from(
-    select t.code,t.name,t.geometry tgeom,c.geometry cgeom from dev-ind-geo-01.geoprocessed.habitation c,("""+level_query+""") t
-    where ST_CONTAINS(t.geometry,h.geometry)
-    )
-    group by code,name) a right join ("""+level_query+""") t on t.code = a.code
-    """
+
     pass
 elif topic == 'Facilities':
-    run_query = """
-    select a.*,t.geometry 
-    from (
-    select code,name,count(cgeom) cnt
-    from(
-    select t.code,t.name,t.geometry tgeom,c.geometry cgeom from dev-ind-geo-01.geoprocessed.facilities c,("""+level_query+""") t
-    where ST_CONTAINS(t.geometry,h.geometry)
-    )
-    group by code,name) a right join ("""+level_query+""") t on t.code = a.code
-    """
+
     pass
 elif topic == 'Proposals':
-    run_query = """
-    select a.*,t.geometry 
-    from (
-    select code,name,count(cgeom) cnt
-    from(
-    select t.code,t.name,t.geometry tgeom,c.geometry cgeom from dev-ind-geo-01.geoprocessed.proposal c,("""+level_query+""") t
-    where ST_CONTAINS(t.geometry,h.geometry)
-    )
-    group by code,name) a right join ("""+level_query+""") t on t.code = a.code
-    """
+
     pass
 elif topic == 'Buildings':
-    run_query = """
-    select a.*,t.geometry 
-    from (
-    select code,name,count(cgeom) cnt
-    from(
-    select t.code,t.name,t.geometry tgeom,c.geometry cgeom from dev-ind-geo-01.buildings.india c,("""+level_query+""") t
-    where ST_CONTAINS(t.geometry,h.geometry)
-    )
-    group by code,name) a right join ("""+level_query+""") t on t.code = a.code
-    """
+
     pass
 elif topic == 'OpenStreetMap PoIs':
-    run_query = """
-    select a.*,t.geometry 
-    from (
-    select code,name,count(cgeom) cnt
-    from(
-    select t.code,t.name,t.geometry tgeom,c.geometry cgeom from dev-ind-geo-01.geoprocessed.road c,("""+level_query+""") t
-    where ST_CONTAINS(t.geometry,h.geometry)
-    )
-    group by code,name) a right join ("""+level_query+""") t on t.code = a.code
-    """
+
     pass
 
-# parse query outputs
-# st.write('select * from table where level=',level,' and topic=',topic)
-st.write(level_query)
-st.write(run_query)
 
 # activate map with button ?
 m = leafmap.Map(minimap_control=True)
